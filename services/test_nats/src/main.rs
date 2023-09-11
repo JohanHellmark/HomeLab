29452563969b1641e72f773a1ebfb516895f4d39
Johan Hellmark <johan.hellmark50@gmial.com>
2023-09-11 21:38:32 +0200

    changed action

--- services/test_nats/src/main.rs
+++ services/test_nats/src/main.rs
@@ -2,7 +2,7 @@ fn main() -> std::io::Result<()> {
     let nc = nats::connect("10.233.18.151")?;
 
     // Using a threaded handler.
-    na.subscribe("bar")?.with_handler(move |msg| {
+    nc.subscribe("bar")?.with_handler(move |msg| {
         println!("Received {}", &msg);
         Ok(())
     });

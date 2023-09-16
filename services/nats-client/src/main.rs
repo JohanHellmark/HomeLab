f1bf30822c69ae0e90ac6b9dff6c6d5a39c5822d
Johan Hellmark <johan.hellmark50@gmial.com>
2023-09-16 15:25:28 +0200

    Added sleep

--- services/nats-client/src/main.rs
+++ services/nats-client/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::{thread, time::Duration};
 
 fn main() -> io::Result<()> {
     println!("Hello, world!");
@@ -6,6 +7,7 @@ fn main() -> io::Result<()> {
     let mut counter: i32 = 0;
     loop {
         counter = counter + 1;
+        thread::sleep(Duration::from_secs(10));
         nc.publish("bar", format!("Sending You A Message: {}", counter))?;
         println!("{}", counter)
     }

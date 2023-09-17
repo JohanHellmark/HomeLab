3da24a64e0ea62fc5f81afe2df51449797676a47
Johan Hellmark <johan.hellmark50@gmial.com>
2023-09-17 09:40:44 +0200

    test

--- services/dice-roller/dice_roller/main.py
+++ services/dice-roller/dice_roller/main.py
@@ -5,10 +5,11 @@ import random
 tracer = trace.get_tracer("diceroller.tracer")
 
 def do_roll():
-    with tracer.start_as_current_span("do_roll") as rollspan:
-        res = random.randint(1, 6)
-        rollspan.set_attribute("roll.value", res)
-        return res
+    while True: 
+        with tracer.start_as_current_span("do_roll") as rollspan:
+            res = random.randint(1, 6)
+            rollspan.set_attribute("roll.value", res)
+        time.sleep(10)
 
 if __name__ == "__main__":
     do_roll()

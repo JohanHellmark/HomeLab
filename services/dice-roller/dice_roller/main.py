3f2691adea55a8470c8ff3e7b49a234890dc11db
Johan Hellmark <johan.hellmark50@gmial.com>
2023-09-17 14:13:29 +0200

    test

--- services/dice-roller/dice_roller/main.py
+++ services/dice-roller/dice_roller/main.py
@@ -1,15 +1,31 @@
 import time
 from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
 import random
 
-tracer = trace.get_tracer("diceroller.tracer")
+
+# Service name is required for most backends,
+# and although it's not necessary for console export,
+# it's good to set service name anyways.
+resource = Resource(attributes={
+    SERVICE_NAME: "your-service-name"
+})
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
 
 def do_roll():
+    tracer = trace.get_tracer("my.tracer")
     while True: 
         with tracer.start_as_current_span("do_roll") as rollspan:
             res = random.randint(1, 6)
             rollspan.set_attribute("roll.value", res)
-        time.sleep(10)
+        time.sleep(2)
 
 if __name__ == "__main__":
     do_roll()

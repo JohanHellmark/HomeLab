93c89feaa22d62a4424f0a6dfdf78c03f0241024
Johan Hellmark <johan.hellmark50@gmial.com>
2023-11-19 16:36:22 +0100

    test

--- infrastructure/terraform/vms/virtual_machines.tf
+++ infrastructure/terraform/vms/virtual_machines.tf
@@ -93,3 +93,99 @@ resource "proxmox_virtual_environment_vm" "k8s_worker_01" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "k8s_worker_02" {
+  name        = "k8s-worker-02"
+  description = "Managed by Terraform"
+  tags        = ["terraform", "ubuntu"]
+  node_name   = "proxmox"
+
+  cpu {
+    cores = 3
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  agent {
+    enabled = true
+  }
+
+  disk {
+    datastore_id = "local"
+    interface    = "scsi0"
+    file_id      = proxmox_virtual_environment_file.ubuntu_container_template.id
+    ssd          = true
+    size         = 30
+  }
+
+  operating_system {
+    type = "l26" # Linux Kernel 2.6 - 5.X.
+  }
+
+
+  initialization {
+    datastore_id      = "local"
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "k8s_worker_03" {
+  name        = "k8s-worker-03"
+  description = "Managed by Terraform"
+  tags        = ["terraform", "ubuntu"]
+  node_name   = "proxmox"
+
+  cpu {
+    cores = 3
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  agent {
+    enabled = true
+  }
+
+  disk {
+    datastore_id = "local"
+    interface    = "scsi0"
+    file_id      = proxmox_virtual_environment_file.ubuntu_container_template.id
+    ssd          = true
+    size         = 30
+  }
+
+  operating_system {
+    type = "l26" # Linux Kernel 2.6 - 5.X.
+  }
+
+
+  initialization {
+    datastore_id      = "local"
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+}
+
